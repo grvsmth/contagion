1a60a8ba43707fd41e0f3b82235dd8c8baa013b0
Angus Grieve-Smith <angus@grieve-smith.com>
2024-10-20 20:18:52 +0000

    Initial commit

---
+++ contagion/django_environ.py
@@ -0,0 +1,29 @@
+"""
+
+Environment variables for Django
+
+"""
+from json import dumps
+from os import environ
+
+def set_environ():
+    """
+    Update the environment variables
+    """
+    environ.update({
+        'DJANGO_SECRET_KEY': 'django-insecure-2f^ii&xc5qjin7fk$@4abk!wm(63owm#b#=_)8*1bmr82cj_0_',
+        'DJANGO_HOST': 'contation.grieve-smith.com',
+        'DJANGO_DB': 'grvsmth$contagion',
+        'DJANGO_DB_USER': 'grvsmth',
+        'DJANGO_DB_PASSWORD': 'myAnnotate',
+        'DJANGO_DB_HOST': 'grvsmth.mysql.pythonanywhere-services.com',
+        'DJANGO_DB_PORT': '3306',
+        'DJANGO_DB_ENGINE': 'django.db.backends.mysql',
+        'DJANGO_DB_OPTIONS': dumps({
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }),
+        'DJANGO_ALLOWED_HOSTS': '[]',
+        'DJANGO_TIMEZONE': "America/New_York",
+        'JWT_EXPIRATION': '2',
+        'JWT_EXPIRATION_UNITS': 'days'
+        })
