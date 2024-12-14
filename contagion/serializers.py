632af12baadcd59cba281c20ae550bd7a8e466af
Angus Grieve-Smith <angus@grieve-smith.com>
2024-12-14 21:50:34 +0000

    Display statistics

--- contagion/serializers.py
+++ contagion/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import Group, User
 from rest_framework.serializers import (
-    HyperlinkedModelSerializer, HyperlinkedRelatedField, PrimaryKeyRelatedField
+    HyperlinkedModelSerializer, PrimaryKeyRelatedField
 )
 
 from contagion.models import Locality, DayData
--- static_staging/js/main.js
+++ static_staging/js/main.js
@@ -8,3 +8,18 @@ const cacheClient = new CacheClient();
 const dayData = await cacheClient.fetchData(dayDataUrl);
 
 console.log("dayData", dayData);
+
+const latestDayData = dayData[dayData.length - 1];
+const latestComplete = dayData.findLast((dayInfo) => {
+    return !dayInfo.incomplete;
+});
+
+document.querySelector("#nyc-hosp-7day").innerText = latestDayData
+    .hosp_count_7day_avg;
+document.querySelector("#nyc-hosp-7day-complete").innerText = latestComplete
+    .hosp_count_7day_avg;
+
+document.querySelector("#nyc-cases-7day").innerText = latestDayData
+    .all_case_count_7day_avg;
+document.querySelector("#nyc-cases-7day-complete").innerText = latestComplete
+    .all_case_count_7day_avg;
